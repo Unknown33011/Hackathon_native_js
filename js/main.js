7446760c008066027e06898945de58ef946020c8
Сыймык Джумагулов <jumagulovsyimyk0404@gmail.com>
2023-10-16 19:48:34 +0600

    hackathon

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-/* этот код помечает картинки, для удобства разработки */
 let i = 1;
 for (let li of carousel.querySelectorAll("li")) {
   li.style.position = "relative";
@@ -9,33 +8,28 @@ for (let li of carousel.querySelectorAll("li")) {
   i++;
 }
 
-/* конфигурация */
-let width = 380; // ширина картинки
-let count = 1; // видимое количество изображений
+let width = 380;
+let count = 1;
 
 let list = carousel.querySelector("ul");
 let listElems = carousel.querySelectorAll("li");
 
-let position = 0; // положение ленты прокрутки
+let position = 0;
 
 carousel.querySelector(".prev").onclick = function () {
-  // сдвиг влево
   position += width * count;
-  // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+
   position = Math.min(position, 0);
   list.style.marginLeft = position + "px";
 };
 
 carousel.querySelector(".next").onclick = function () {
-  // сдвиг вправо
   position -= width * count;
-  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+
   position = Math.max(position, -width * (listElems.length - count));
   list.style.marginLeft = position + "px";
 };
 
-/* этот код помечает картинки, для удобства разработки */
-// let blockSale = block_sale.querySelector(".block_sale");
 let j = 1;
 for (let li of block_sale.querySelectorAll("li")) {
   li.style.position = "relative";
@@ -46,27 +40,22 @@ for (let li of block_sale.querySelectorAll("li")) {
   j++;
 }
 
-/* конфигурация */
-let widthSale = 380; // ширина картинки
-let countSale = 1; // видимое количество изображений
+let widthSale = 380;
+let countSale = 1;
 
 let listSale = block_sale.querySelector("ul");
 let listElemsSale = block_sale.querySelectorAll("li");
 
-let positionSale = 0; // положение ленты прокрутки
+let positionSale = 0;
 
 block_sale.querySelector(".prev").onclick = function () {
-  // сдвиг влево
   positionSale += widthSale * countSale;
-  // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
   positionSale = Math.min(positionSale, 0);
   listSale.style.marginLeft = positionSale + "px";
 };
 
 block_sale.querySelector(".next").onclick = function () {
-  // сдвиг вправо
   positionSale -= widthSale * countSale;
-  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
   positionSale = Math.max(
     positionSale,
     -widthSale * (listElemsSale.length - countSale)
